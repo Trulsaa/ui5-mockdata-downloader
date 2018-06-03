5cba61b0e0ca75f294c88a87d8d5fe98d1379656
Truls Aagaard <truls.aa@gmail.com>
2018-06-03 21:44:52 +0200

    Make run async

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const manifest = require(process.cwd() + "/webapp/manifest.json");
 
 require("dotenv").config();
 
-const run = () => {
+const run = async () => {
   const oSources = manifest["sap.app"].dataSources,
     aSourceNames = Object.keys(oSources);
 
   for (const source of aSourceNames) {
-    downloadAllFromSource(oSources[source]);
+    await downloadAllFromSource(oSources[source]);
   }
 };
 
